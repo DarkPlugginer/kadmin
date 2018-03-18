0468da77c8bb7d011ba5f320f28dac718fdb7ea0
miguel <miguellukas52@gmail.com>
2018-03-18 20:46:07 -0300

    Cop

--- src/main/kotlin/me/dark/Main.kt
+++ src/main/kotlin/me/dark/Main.kt
@@ -29,6 +29,7 @@ class Main : JavaPlugin() {
 
     override fun onEnable() {
         instance = this
+        println()
         val craftServer = server as CraftServer
 
         val getter = ClassGetter(this)
